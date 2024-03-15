3eed61d3f8d1dd393f4285808dd91504c05109bd
Mima <gx_339-4@pm.me>
2024-03-15 12:12:32 +0100

    Add MainActivity

--- android/src/main/kotlin/org/acyb/sayit/app/MainActivity.kt
+++ android/src/main/kotlin/org/acyb/sayit/app/MainActivity.kt
@@ -1,10 +1,4 @@
-/*
- * Copyright (c) 2024 Matthias Geisler (bitPogo) / All rights reserved.
- *
- * Use of this source code is governed by Apache v2.0
- */
-
-package tech.antibytes.project.example.app
+package org.acyb.sayit.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +6,5 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TODO()
     }
 }
