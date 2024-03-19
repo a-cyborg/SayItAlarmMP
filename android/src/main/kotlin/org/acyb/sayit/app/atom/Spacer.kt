9e41e53cf4e3ebc0232e5201d6f9a7861541d5d0
Mima <gx_339-4@pm.me>
2024-03-19 14:49:50 +0900

    Add Spacing, Spacer UI components

---
+++ android/src/main/kotlin/org/acyb/sayit/app/atom/Spacer.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.atom
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.acyb.sayit.app.token.Spacing
+
+@Composable
+fun SpacerLarge() {
+    Spacer(modifier = Modifier.size(Spacing.l))
+}
---
+++ android/src/main/kotlin/org/acyb/sayit/app/token/Spacing.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.token
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val none = 0.dp
+    val xxxs = 3.dp
+    val xxs = 6.dp
+    val xs = 9.dp
+    val s = 12.dp
+    val m = 18.dp
+    val l = 24.dp
+    val xl = 30.dp
+    val xxl = 39.dp
+    val xxxl = 41.dp
+    val xxxxl = 51.dp
+}
