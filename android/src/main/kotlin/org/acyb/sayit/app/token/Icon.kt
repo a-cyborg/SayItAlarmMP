1d6595aa691130b2009c73b95bfda72f5fcd82aa
Mima <gx_339-4@pm.me>
2024-03-19 14:52:43 +0900

    Add edit icon

--- android/src/main/kotlin/org/acyb/sayit/app/token/Icon.kt
+++ android/src/main/kotlin/org/acyb/sayit/app/token/Icon.kt
@@ -7,12 +7,15 @@
 package org.acyb.sayit.app.token
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material.icons.rounded.Settings
 
 object Icon {
     val add = Icons.Rounded.Add
+    val editArrowRight = Icons.Rounded.KeyboardArrowRight
     val Settings = Icons.Rounded.Settings
     val delete = Icons.Rounded.Delete
 }
