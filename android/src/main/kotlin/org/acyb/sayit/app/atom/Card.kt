254cb9db65a83eea90254c704f6e304507f01520
Mima <gx_339-4@pm.me>
2024-03-25 20:26:42 +0900

    Add Card atom component

---
+++ android/src/main/kotlin/org/acyb/sayit/app/atom/Card.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.atom
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.acyb.sayit.app.token.Color
+import org.acyb.sayit.app.token.Elevation
+import org.acyb.sayit.app.token.Shape
+import org.acyb.sayit.app.token.Spacing
+
+@Composable
+fun CardStandard(content: @Composable () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Spacing.m, vertical = Spacing.l),
+        shape = Shape.Card.primary,
+        backgroundColor = Color.surface.subtle,
+        elevation = Elevation.thirdLevel,
+    ) {
+        content()
+    }
+}
