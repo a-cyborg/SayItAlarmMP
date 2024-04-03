80c71ab539e6ff42a842792a7e80c1505f4262db
Mima <gx_339-4@pm.me>
2024-04-03 23:32:02 +0900

    Add surface color tests

--- android/src/test/kotlin/org/acyb/sayit/app/token/ColorSpec.kt
+++ android/src/test/kotlin/org/acyb/sayit/app/token/ColorSpec.kt
@@ -6,52 +6,31 @@
 
 package org.acyb.sayit.app.token
 
-import androidx.compose.ui.graphics.Color as ColorPalette
 import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.DynamicTest.*
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 import tech.antibytes.util.test.mustBe
+import androidx.compose.ui.graphics.Color as ColorPalette
 
 class ColorSpec {
     @TestFactory
     fun `Color is initially in LightMode`(): List<DynamicTest> {
         return listOf(
-            ColorModeTestSpec(
-                "text.standard",
-                ColorPalette(0xFF000000)
-            ) { Color.text.standard },
-            ColorModeTestSpec(
-                "text.subtle",
-                ColorPalette(0xFF7C6F64),
-            ) { Color.text.subtle },
-            ColorModeTestSpec(
-                "text.inverse",
-                ColorPalette(0xFFFFFFFF),
-            ) { Color.text.inverse },
-            ColorModeTestSpec(
-                "text.success",
-                ColorPalette(0xFF79740E),
-            ) { Color.text.success },
-            ColorModeTestSpec(
-                "text.info",
-                ColorPalette(0xFF076678),
-            ) { Color.text.info },
-            ColorModeTestSpec(
-                "text.warning",
-                ColorPalette(0xFF8F3F71),
-            ) { Color.text.warning },
-            ColorModeTestSpec(
-                "text.danger",
-                ColorPalette(0xFF9D0006),
-            ) { Color.text.danger },
-            ColorModeTestSpec(
-                "text.accent",
-                ColorPalette(0xFFB57614),
-            ) { Color.text.accent },
-            ColorModeTestSpec(
-                "text.attention",
-                ColorPalette(0xFF427658),
-            ) { Color.text.attention },
+            ColorModeTestSpec("text.standard", ColorPalette(0xFF000000)) { Color.text.standard },
+            ColorModeTestSpec("text.subtle", ColorPalette(0xFF7C6F64)) { Color.text.subtle },
+            ColorModeTestSpec("text.inverse", ColorPalette(0xFFFFFFFF)) { Color.text.inverse },
+            ColorModeTestSpec("text.success", ColorPalette(0xFF79740E)) { Color.text.success },
+            ColorModeTestSpec("text.info", ColorPalette(0xFF076678)) { Color.text.info },
+            ColorModeTestSpec("text.warning", ColorPalette(0xFF8F3F71)) { Color.text.warning },
+            ColorModeTestSpec("text.danger", ColorPalette(0xFF9D0006)) { Color.text.danger },
+            ColorModeTestSpec("text.accent", ColorPalette(0xFFB57614)) { Color.text.accent },
+            ColorModeTestSpec("text.attention", ColorPalette(0xFF427658)) { Color.text.attention },
+            ColorModeTestSpec("surface.standard", ColorPalette(0xFFFFFFFF)) { Color.surface.standard },
+            ColorModeTestSpec("surface.subtle", ColorPalette(0xFFA89984)) { Color.surface.subtle },
+            ColorModeTestSpec("surface.strong", ColorPalette(0xFF928374)) { Color.surface.strong },
+            ColorModeTestSpec("surface.success", ColorPalette(0xFF79740E)) { Color.surface.success },
+            ColorModeTestSpec("surface.accent", ColorPalette(0xFFB57614)) { Color.surface.accent },
+            ColorModeTestSpec("surface.attention", ColorPalette(0xFF427658)) { Color.surface.attention },
         ).map { (descriptor, expected, actual) ->
             dynamicTest("Color.$descriptor is initially in LightMode") {
                 actual() mustBe expected
@@ -65,44 +44,23 @@ class ColorSpec {
         Color.useDarkTheme()
 
         return listOf(
-            ColorModeTestSpec(
-                "text.standard",
-                ColorPalette(0xFFFFFFFF)
-            ) { Color.text.standard },
-            ColorModeTestSpec(
-                "text.subtle",
-                ColorPalette(0xFFA89984),
-            ) { Color.text.subtle },
-            ColorModeTestSpec(
-                "text.inverse",
-                ColorPalette(0xFF000000),
-            ) { Color.text.inverse },
-            ColorModeTestSpec(
-                "text.success",
-                ColorPalette(0xFF98971A),
-            ) { Color.text.success },
-            ColorModeTestSpec(
-                "text.info",
-                ColorPalette(0xFF458588),
-            ) { Color.text.info },
-            ColorModeTestSpec(
-                "text.warning",
-                ColorPalette(0xFFB16286),
-            ) { Color.text.warning },
-            ColorModeTestSpec(
-                "text.danger",
-                ColorPalette(0xFFCC241D),
-            ) { Color.text.danger },
-            ColorModeTestSpec(
-                "text.accent",
-                ColorPalette(0xFFD79921),
-            ) { Color.text.accent },
-            ColorModeTestSpec(
-                "text.attention",
-                ColorPalette(0xFF689D6A),
-            ) { Color.text.attention },
+            ColorModeTestSpec("text.standard", ColorPalette(0xFFFFFFFF)) { Color.text.standard },
+            ColorModeTestSpec("text.subtle", ColorPalette(0xFFA89984)) { Color.text.subtle },
+            ColorModeTestSpec("text.inverse", ColorPalette(0xFF000000)) { Color.text.inverse },
+            ColorModeTestSpec("text.success", ColorPalette(0xFF98971A)) { Color.text.success },
+            ColorModeTestSpec("text.info", ColorPalette(0xFF458588)) { Color.text.info },
+            ColorModeTestSpec("text.warning", ColorPalette(0xFFB16286)) { Color.text.warning },
+            ColorModeTestSpec("text.danger", ColorPalette(0xFFCC241D)) { Color.text.danger },
+            ColorModeTestSpec("text.accent", ColorPalette(0xFFD79921)) { Color.text.accent },
+            ColorModeTestSpec("text.attention", ColorPalette(0xFF689D6A)) { Color.text.attention },
+            ColorModeTestSpec("surface.standard", ColorPalette(0xFF282828)) { Color.surface.standard },
+            ColorModeTestSpec("surface.subtle", ColorPalette(0xFF7C6F64)) { Color.surface.subtle },
+            ColorModeTestSpec("surface.strong", ColorPalette(0xFF000000)) { Color.surface.strong },
+            ColorModeTestSpec("surface.success", ColorPalette(0xFF98971A)) { Color.surface.success },
+            ColorModeTestSpec("surface.accent", ColorPalette(0xFFD79921)) { Color.surface.accent },
+            ColorModeTestSpec("surface.attention", ColorPalette(0xFF689D6A)) { Color.surface.attention },
         ).map { (descriptor, expected, actual) ->
-            dynamicTest("Color.$descriptor is initially in LightMode") {
+            dynamicTest("When useDarkMode is called Color.$descriptor is in DarkMode") {
                 actual() mustBe expected
             }
         }
@@ -114,44 +72,23 @@ class ColorSpec {
         Color.useLightTheme()
 
         return listOf(
-            ColorModeTestSpec(
-                "text.standard",
-                ColorPalette(0xFF000000)
-            ) { Color.text.standard },
-            ColorModeTestSpec(
-                "text.subtle",
-                ColorPalette(0xFF7C6F64),
-            ) { Color.text.subtle },
-            ColorModeTestSpec(
-                "text.inverse",
-                ColorPalette(0xFFFFFFFF),
-            ) { Color.text.inverse },
-            ColorModeTestSpec(
-                "text.success",
-                ColorPalette(0xFF79740E),
-            ) { Color.text.success },
-            ColorModeTestSpec(
-                "text.info",
-                ColorPalette(0xFF076678),
-            ) { Color.text.info },
-            ColorModeTestSpec(
-                "text.warning",
-                ColorPalette(0xFF8F3F71),
-            ) { Color.text.warning },
-            ColorModeTestSpec(
-                "text.danger",
-                ColorPalette(0xFF9D0006),
-            ) { Color.text.danger },
-            ColorModeTestSpec(
-                "text.accent",
-                ColorPalette(0xFFB57614),
-            ) { Color.text.accent },
-            ColorModeTestSpec(
-                "text.attention",
-                ColorPalette(0xFF427658),
-            ) { Color.text.attention }, // TODO: Add surface after decide final surface color
+            ColorModeTestSpec("text.standard", ColorPalette(0xFF000000)) { Color.text.standard },
+            ColorModeTestSpec("text.subtle", ColorPalette(0xFF7C6F64)) { Color.text.subtle },
+            ColorModeTestSpec("text.inverse", ColorPalette(0xFFFFFFFF)) { Color.text.inverse },
+            ColorModeTestSpec("text.success", ColorPalette(0xFF79740E)) { Color.text.success },
+            ColorModeTestSpec("text.info", ColorPalette(0xFF076678)) { Color.text.info },
+            ColorModeTestSpec("text.warning", ColorPalette(0xFF8F3F71)) { Color.text.warning },
+            ColorModeTestSpec("text.danger", ColorPalette(0xFF9D0006)) { Color.text.danger },
+            ColorModeTestSpec("text.accent", ColorPalette(0xFFB57614)) { Color.text.accent },
+            ColorModeTestSpec("text.attention", ColorPalette(0xFF427658)) { Color.text.attention },
+            ColorModeTestSpec("surface.standard", ColorPalette(0xFFFFFFFF)) { Color.surface.standard },
+            ColorModeTestSpec("surface.subtle", ColorPalette(0xFFA89984)) { Color.surface.subtle },
+            ColorModeTestSpec("surface.strong", ColorPalette(0xFF928374)) { Color.surface.strong },
+            ColorModeTestSpec("surface.success", ColorPalette(0xFF79740E)) { Color.surface.success },
+            ColorModeTestSpec("surface.accent", ColorPalette(0xFFB57614)) { Color.surface.accent },
+            ColorModeTestSpec("surface.attention", ColorPalette(0xFF427658)) { Color.surface.attention },
         ).map { (descriptor, expected, actual) ->
-            dynamicTest("Color.$descriptor is initially in LightMode") {
+            dynamicTest("When useLightMode is called Color.$descriptor is in LightMode") {
                 actual() mustBe expected
             }
         }
