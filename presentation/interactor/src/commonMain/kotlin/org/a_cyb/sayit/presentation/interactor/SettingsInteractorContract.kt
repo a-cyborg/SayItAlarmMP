d44b2c8daa8400be918c4e84cd7427ef8cb629c4
Mima <gx_339-4@pm.me>
2024-04-10 18:19:06 +0900

    Remove SetTimeOut, SetSnooze abstract function from SettingsInteractorContract

---
+++ presentation/interactor/src/commonMain/kotlin/org/a_cyb/sayit/presentation/interactor/SettingsInteractorContract.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.a_cyb.sayit.presentation.interactor
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharedFlow
+import org.a_cyb.sayit.entity.Settings
+
+interface SettingsInteractorContract {
+    val settings: SharedFlow<Result<Settings>>
+
+    fun save(settings: Settings, scope: CoroutineScope)
+}
