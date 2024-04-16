c7e5789419ccbab994f098b3b1106ebd6dde0e8b
Mima <gx_339-4@pm.me>
2024-04-16 19:06:36 +0900

    Add antibytes publishing plugtin on entity module

--- entity/build.gradle.kts
+++ entity/build.gradle.kts
@@ -10,6 +10,7 @@ import tech.antibytes.gradle.configuration.sourcesets.iosx
 plugins {
     alias(antibytesCatalog.plugins.gradle.antibytes.kmpConfiguration)
     alias(antibytesCatalog.plugins.gradle.antibytes.androidLibraryConfiguration)
+    alias(antibytesCatalog.plugins.gradle.antibytes.publishing)
 }
 
 val projectPackage = "org.acyb.sayit.entity"
