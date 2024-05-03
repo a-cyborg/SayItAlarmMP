113b87a295866e1830b4ca10884ed1203006aafb
Mima <gx_339-4@pm.me>
2024-05-04 00:45:23 +0900

    Add ActionRowCancelAndConfirm

---
+++ android/src/debug/kotlin/org/acyb/sayit/app/molecule/ActionRowPreview.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.molecule
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun ActionRowCancelAndConfirmPreview() {
+    ActionRowCancelAndConfirm(
+        onCancel = {},
+        onConfirm = {},
+    )
+}
---
+++ android/src/test/kotlin/org/acyb/sayit/app/molecule/ActionRowSpec.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.molecule
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.acyb.sayit.app.RoborazziTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [33])
+class ActionRowSpec : RoborazziTest() {
+
+    @Test
+    fun `It renders ActionRowCancelAndConfirm`() {
+        subjectUnderTest.setContent {
+            ActionRowCancelAndConfirm(
+                onCancel = {},
+                onConfirm = {}
+            )
+        }
+    }
+}
