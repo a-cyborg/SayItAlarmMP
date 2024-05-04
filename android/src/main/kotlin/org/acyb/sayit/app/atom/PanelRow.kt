fd02dd39e2cdf8ee88b438e1549055ae2fa85e40
Mima <gx_339-4@pm.me>
2024-05-04 14:13:26 +0900

    Add PanelRow UI component

---
+++ android/src/main/kotlin/org/acyb/sayit/app/atom/PanelRow.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.atom
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PanelRowStandard(
+    valueLabel: String,
+    value: String = "",
+    afterContent: @Composable () -> Unit = {},
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        TextBodyStandardLarge(text = valueLabel)
+        Spacer(Modifier.weight(1f))
+        TextBodySubtleLarge(text = value)
+        afterContent()
+    }
+}
---
+++ android/src/test/kotlin/org/acyb/sayit/app/atom/PanelRowSpec.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.atom
+
+import androidx.compose.ui.res.stringResource
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.acyb.sayit.R
+import org.acyb.sayit.app.RoborazziTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [33])
+class PanelRowSpec : RoborazziTest() {
+
+    @Test
+    fun `It renders PanelRowStandard`() {
+        subjectUnderTest.setContent {
+            PanelRowStandard(
+                valueLabel = stringResource(id = R.string.timeout),
+                value = stringResource(id = R.string.minute_short, 180),
+                afterContent = { IconButtonEditNoPadding {} }
+            )
+        }
+    }
+
+    @Test
+    fun `Given PanelRowSpec displays without a value`() {
+        subjectUnderTest.setContent {
+            PanelRowStandard(
+                valueLabel = stringResource(id = R.string.about),
+                afterContent = { IconButtonEditNoPadding {} }
+            )
+        }
+    }
+
+    @Test
+    fun `Given PanelRowSpec displays without an afterContent`() {
+        subjectUnderTest.setContent {
+            PanelRowStandard(
+                valueLabel = stringResource(id = R.string.version),
+                value = "1.0"
+            )
+        }
+    }
+}
