60017432733ddd724c1a8197ea31fd4988e50b86
Mima <gx_339-4@pm.me>
2024-05-04 14:36:57 +0900

    Add Panel and update screenshots

---
+++ android/src/main/kotlin/org/acyb/sayit/app/atom/Panel.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Mima Kang / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package org.acyb.sayit.app.atom
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.acyb.sayit.app.token.Color
+import org.acyb.sayit.app.token.Shape
+import org.acyb.sayit.app.token.Spacing
+
+@Composable
+fun PanelStandard(
+    panelItems: List<@Composable () -> Unit>,
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color = Color.surface.standard, shape = Shape.PanelSection.primary)
+            .padding(horizontal = Spacing.m),
+    ) {
+        panelItems.forEach { item ->
+            SpacerMedium()
+            item()
+            DividerStandard()
+        }
+        SpacerMedium()
+    }
+}
